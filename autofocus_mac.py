6935cdc3fd17cc435ec173b1c8fc87857660c8c5
Cristina <131235866+cfich4@users.noreply.github.com>
2025-06-14 01:58:43 -0400

    closest to accurate version

--- autofocus_mac.py
+++ autofocus_mac.py
@@ -1,3 +1,17 @@
+# SDM: create a program that makes a list from user input. This program is to organize the list by what
+# the user deems top priority. This program will have four tasks:
+# 1) add an item
+# 2) prioritize an item
+# 3) mark an item complete
+# 4) delete an item (ask about including)
+
+#[x] done
+#[o] ready
+#[ ] new
+
+#for delete_list function (add that), you'd want to add a while loop to have it repeatedly delete items
+#in a list.
+
 print("Welcome to the To-Do List App")
 
 todo_list = []
@@ -21,12 +35,12 @@ def display_list():
         priority = task[1]
         name = task[2]
         
-        if (status == "[ ]"):
+        if (status == "[O]"):
             status_value = 0
-        elif (status == "[O]"):
+        elif (status == "[X]"):
             status_value = 1
         else: 
-            # (status == "[X]")
+            # (status == "[]")
             status_value = 2
 
         #using Chatgpt to assist with my bugs, I learned that I needed to parethenteses because
@@ -46,24 +60,41 @@ def display_list():
 
     print()
 
-def add_item(task):
+def add_item():
     '''This function creates the ability to allow the user to create a new task after they enter
     'add' in the terminal.'''
 #    todo_list.append(task)
-    name = input("Please type out your new item: ")
+    new_item = input("Please type out your new item: ")
     prioritizing = True
     while prioritizing:
-        priority_input = input("1 is the highest priority")
+        #I've tried to incorporate the bracket system with a number system and they are connecting,
+        #just not in the way I've been trying. I've been trying everything to fix it but I am rendered
+        #in capable. 
+        priority_input = input("Do you want to prioritize this? Entering 1 will put" \
+        " it at the highest. ")
+        #I believed that making it numbers with .isdigit would help the user see something like this:
+        # >beans (let's say this is user input)
+        # Do you want to prioritize this? Entering 1 will place it the highest
+        # [O] beans
+        #no matter how much I change around the numbers, I only get [ ]s
+        #at the very least, it does rearrange where to put the priority. Example, if you put
+        #fish as 2 and then cat as 1, cat will appear first.
         if priority_input.isdigit():
             priority = int(priority_input)
             prioritizing = False
         print("Please enter a number.")
-    todo_list.append(("[ ]", priority, name, task))
+        #Chat gpt taught me to add "[ ]" as it adds the string.
+    todo_list.append(("[ ]", priority, new_item))
        
-
-def remove_list(list):
+def remove_list():
     '''This function is to remove the entire list at the user's command.'''
-    todo_list.remove(list)
+    # todo_list.remove(list)
+    confirm = input("Are you sure you want to clear all tasks? (yes/no): ")
+    if confirm == "yes":
+        todo_list.clear()
+        print("All tasks removed.")
+    else:
+        print("Cancelled.")
 
 def prioritize_task():
     '''This function prioritizes tasks throughout the list by asking the user which number (it seems easier) they
@@ -107,38 +138,17 @@ running = True
 while running:
     print("Please enter 'add'(to add a task), 'delete' if you want to remove the list, or"
    " 'q' (to quit)") 
-# or type a number to remove an item: ")
     answer = input("> ")
    # if the user enters 'q':
     if (answer == "q"):
        print(f"Goodbye. Good luck on your tasks of {answer}.")
-       # TODO: this renders 'answer' instead of a given task
        running = False
     elif (answer == "delete"):
        display_list()
-       remove_list(list)
-   # this is to create a list for the user
-#    elif (answer.isnumeric()):
-#        index = int(answer) - 1
-#        # to ensure a user is not trying to remove nothing from a list with no items
-#        if (len(todo_list) == 0):
-#            print("There is nothing to remove. Your list is empty.")
-#        else:
-#            # for removing a singular item
-#            if (index >= 0 and index < len(todo_list)):
-#                todo_list.pop(index)
-#            else:
-#                print("Sorry, there isn't an item with that number. ")
-   # this processes the user's inputted item
+       remove_list()
     elif (answer == "add"):
-       add_item(input("Please type out your new item: "))
+       add_item()
        display_list()
    # if the user enters nothing
     else:
        print("Please enter an item or enter 'q' to quit.")
-
-
-   # to number order and repeat the list to the user before they leave the program'
-# for num in range(len(todo_list)):
-#        print(f"{num + 1}. {todo_list[num]}")
-    #might move to prioritization list
